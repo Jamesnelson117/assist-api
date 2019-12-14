1664ff3dd9eb1976c16a72966b2734a6ef9dd053
James Nelson <Jamesnelson117@gmail.com>
2019-12-14 21:17:11 +0000

    fix: Reenabled * CORS

--- src/util/setupMiddleware.js
+++ src/util/setupMiddleware.js
@@ -9,9 +9,9 @@ const bodyParser = require('body-parser');
 const setupMiddleware = app => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-    
+
   app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'content-type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
